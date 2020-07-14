b77a349a8ce2002f79bb37f036e19835c66054e9
ealmenzar <marina.estal@gmail.com>
2020-07-14 15:38:19 +0200

    Update tetrode.py: preprocess_output for orientation

--- deepinsight/util/tetrode.py
+++ deepinsight/util/tetrode.py
@@ -95,12 +95,12 @@ def preprocess_output(fp_hdf_out, raw_timestamps, output, output_timestamps, ave
         File path to HDF5 file
     raw_timestamps : (N,1) array_like
         Timestamps for each sample in continous
-    output : (N,4) array_like
-        Position of animal with two LEDs
+    output : (N,1) array_like
+        Orientation
     output_timestamps : (N,1) array_like
         Timestamps for positions
     average_window : int, optional
-        Downsampling factor for raw data and positions, by default 1000
+        Downsampling factor for raw data and orientation, by default 1000
     sampling_rate : int, optional
         Sampling rate of raw ephys, by default 30000
     """
@@ -109,36 +109,14 @@ def preprocess_output(fp_hdf_out, raw_timestamps, output, output_timestamps, ave
     # Get size of wavelets
     input_length = hdf5_file['inputs/wavelets'].shape[0]
 
-    # Get positions of both LEDs
+    # Get orientation and calculates alignment to the raw_data with downsampling factor average_window
     raw_timestamps = raw_timestamps[()]  # Slightly faster than np.array
-    output_x_led1 = np.interp(raw_timestamps[np.arange(0, raw_timestamps.shape[0],
-                                                       average_window)], output_timestamps, output[:, 0])
-    output_y_led1 = np.interp(raw_timestamps[np.arange(0, raw_timestamps.shape[0],
-                                                       average_window)], output_timestamps, output[:, 1])
-    output_x_led2 = np.interp(raw_timestamps[np.arange(0, raw_timestamps.shape[0],
-                                                       average_window)], output_timestamps, output[:, 2])
-    output_y_led2 = np.interp(raw_timestamps[np.arange(0, raw_timestamps.shape[0],
-                                                       average_window)], output_timestamps, output[:, 3])
-    raw_positions = np.array([output_x_led1, output_y_led1, output_x_led2, output_y_led2]).transpose()
-
-    # Clean raw_positions and get centre
-    positions_smooth = pd.DataFrame(raw_positions.copy()).interpolate(
-        limit_direction='both').rolling(5, min_periods=1).mean().get_values()
-    position = np.array([(positions_smooth[:, 0] + positions_smooth[:, 2]) / 2,
-                         (positions_smooth[:, 1] + positions_smooth[:, 3]) / 2]).transpose()
-
-    # Also get head direction and speed from positions
-    speed = stats.calculate_speed_from_position(position, interval=1/(sampling_rate//average_window), smoothing=3)
-    head_direction = stats.calculate_head_direction_from_leds(positions_smooth, return_as_deg=False)
-
+    output_orientation = np.interp(raw_timestamps[np.arange(0, raw_timestamps.shape[0],
+                                                       average_window)], output_timestamps, output)  #np.arange with average_window makes a reduction on the data
+    raw_orientation = np.array([output_orientation]).transpose()
+    
     # Create and save datasets in HDF5 File
-    hdf5.create_or_update(hdf5_file, dataset_name="outputs/raw_position",
-                          dataset_shape=[input_length, 4], dataset_type=np.float16, dataset_value=raw_positions[0: input_length, :])
-    hdf5.create_or_update(hdf5_file, dataset_name="outputs/position",
-                          dataset_shape=[input_length, 2], dataset_type=np.float16, dataset_value=position[0: input_length, :])
-    hdf5.create_or_update(hdf5_file, dataset_name="outputs/head_direction", dataset_shape=[
-                          input_length, 1], dataset_type=np.float16, dataset_value=head_direction[0: input_length, np.newaxis])
-    hdf5.create_or_update(hdf5_file, dataset_name="outputs/speed",
-                          dataset_shape=[input_length, 1], dataset_type=np.float16, dataset_value=speed[0: input_length, np.newaxis])
+    hdf5.create_or_update(hdf5_file, dataset_name="outputs/raw_orientation",
+                          dataset_shape=[input_length, 1], dataset_type=np.float16, dataset_value=raw_positions[0: input_length, :])
     hdf5_file.flush()
     hdf5_file.close()
